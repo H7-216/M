d99e93a8391e45ecbd0d10376bccdf2f8fd48fa0
H7-216 <hmoussa006@gmail.com>
2024-06-22 01:55:22 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,48 @@
-// scripts.js
+// Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCl2tmy-LU8KHs2s1BjG2CdmvaOr4E3fvY",
+    authDomain: "saaa-7b92d.firebaseapp.com",
+    projectId: "saaa-7b92d",
+    storageBucket: "saaa-7b92d.appspot.com",
+    messagingSenderId: "464310225000",
+    appId: "464310225000:web:2728c6f3911bdd2cfd53dd"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Document is ready!");
-    // Ajoutez ici tout code JavaScript supplémentaire si nécessaire
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+    // Here you can add code to actually send the message
+});
+
+const chatInput = document.getElementById('chat-input');
+const sendButton = document.getElementById('send-button');
+const messagesContainer = document.getElementById('messages');
+
+sendButton.addEventListener('click', sendMessage);
+chatInput.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+});
+
+function sendMessage() {
+    const messageText = chatInput.value.trim();
+    if (messageText !== '') {
+        firebase.database().ref('messages').push().set({
+            "message": messageText
+        });
+        chatInput.value = '';
+    }
+}
+
+// Listen for new messages
+firebase.database().ref('messages').on('child_added', function(snapshot) {
+    const message = snapshot.val().message;
+    const messageElement = document.createElement('div');
+    messageElement.textContent = message;
+    messagesContainer.appendChild(messageElement);
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
 });
